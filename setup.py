dba59509ddb401b53b392667ee5f7e9e964edcad
Mehmet Eraslan <mehmeteraslan@Mehmet-MacBook-Pro-2.local>
2023-09-05 14:22:21 +0300

    pypi package created

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name='awsx',
+    version='1.0',
+    author='Mert Önengil, Mehmet Eraslan',
+    description='AWS credential management tool',
+    long_description='A Python script for managing AWS credentials.',
+    url='https://github.com/mertongngl/awsx',
+    py_modules=['awsx'],
+    install_requires=[
+        'argparse'
+    ],
+    entry_points={
+        'console_scripts': [
+            'awsx = awsx:main',
+        ],
+    },
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ],
+)
